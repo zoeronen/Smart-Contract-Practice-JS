4be52e74860675d946dc0f0b0b3a5767e58df164
Zoe Ronen <zoeronen@email.com>
2022-09-06 15:09:53 -0400

    hardhat verify and tasks

--- hardhat_simple_storage/hardhat.config.js
+++ hardhat_simple_storage/hardhat.config.js
@@ -1,15 +1,22 @@
 require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
 require("dotenv").config()
+require("./tasks/block-number")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
     rinkeby: {
       url: RINKEBY_RPC_URL,
       accounts: [PRIVATE_KEY],
@@ -22,4 +29,7 @@ module.exports = {
     },
   },
   solidity: "0.8.8",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  }
 };
--- hardhat_simple_storage/scripts/deploy.js
+++ hardhat_simple_storage/scripts/deploy.js
@@ -1,5 +1,5 @@
 // imports
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 // async main
 async function main() {
@@ -10,7 +10,39 @@ async function main() {
   const simpleStorage = await SimpleStorageFactory.deploy()
   await simpleStorage.deployed()
   console.log(`Deployed contract to: ${simpleStorage.address}`)
+  // Do not verify when deploying on hardhat network
+  if((network.config.chainId === 4 || network.config.chainId === 5) && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for block txes...")
+    await simpleStorage.deployTransaction.wait(6)
+    await verify(simpleStorage.address, [])
+  }
+
+  const currentValue = await simpleStorage.retrieve()
+  console.log(`Current Value is: ${currentValue}`)
+
+  // Update the current value
+  const transactionResponse = await simpleStorage.store(7)
+  await transactionResponse.wait(1)
+  const updatedValue = await simpleStorage.retrieve()
+  console.log(`Updated Value is ${updatedValue}`)
 }
+
+// async function verify(contractAddress, args) {
+  const verify = async (contractAddress, args) => {
+    console.log("Verifying contract...")
+    try {
+      await run("verify:verify", {
+        address: contractAddress,
+        constructorArguments: args,
+      })
+    } catch (e) {
+      if (e.message.toLowerCase().includes("already verified")) {
+        console.log("Already Verified!")
+      } else {
+        console.log(e)
+      }
+    }
+  }
 
 // main
 main()
