43e87b06a02d7bcf593ee2022d3258a75ec9a735
Zoe Ronen <zoeronen@email.com>
2022-09-11 04:17:56 -0400

    hardhat fund me tests

--- hardhat_fund_me/contracts/FundMe.sol
+++ hardhat_fund_me/contracts/FundMe.sol
@@ -48,7 +48,7 @@ contract FundMe {
         i_owner = msg.sender;
     }
 
-    /// @notice Funds our contract based on the ETH/USD price
+    /** @notice Funds our contract based on the ETH/USD price */
     function fund() public payable {
         require(
             msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
